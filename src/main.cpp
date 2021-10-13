0cf5b76b1b241fdb93282655598cdc328d9ac2b8
shangchiwu <shangchi537@gmail.com>
2021-10-13 17:57:58 +0800

    first glfw window

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,51 @@
 #include <iostream>
+#include <GLFW/glfw3.h>
+
+static void glfw_error_callback(int error, const char *mesage) {
+    fprintf(stderr, "GLFW Error [%d]: %s\n", error, mesage);
+}
 
 int main(int argc, const char **argv) {
-    std::cout << "Hello" << std::endl;
+
+    // init GLFW
+
+    glfwSetErrorCallback(glfw_error_callback);
+
+    if (!glfwInit()){
+        std::cout << "GLFW initialization failed!" << std::endl;
+        return 1;
+    }
+
+    GLFWwindow *window = glfwCreateWindow(1280, 720, "Advanced Image Processor", nullptr, nullptr);
+
+    if (window == nullptr){
+        std::cout << "GLFW window initialization failed!" << std::endl;
+        glfwTerminate();
+        return 1;
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
+
+    // window loop
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+
+        int display_w;
+        int display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
+        glClearColor(1, 1, 1, 1);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwMakeContextCurrent(window);
+        glfwSwapBuffers(window);
+    }
+
+    // cleanup GLFW
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
     return 0;
 }
